7fa306992709f0c4e2d08ebe3cf0717ebeebce12
BreGernon <117558964+BreGernon@users.noreply.github.com>
2023-10-09 04:10:30 -0700

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con=new mysqli('eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'fi8j09p6dfcs6cmi', 'uuceawv8tsmqralz', 'z8m4k04kc7o8w1d1');
-
-if($con===false){
-    die("ERROR: Could not connect. " .
-        mysqli_connect_error());
-}
-?>
